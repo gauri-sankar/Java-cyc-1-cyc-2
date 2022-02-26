3ab746e07b282695ed0a8571d334894ed6353d23
gauri-sankar <96428549+gauri-sankar@users.noreply.github.com>
2022-02-26 09:25:57 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+class Sum
+{
+	public static void main(String[] args)
+	{
+		int a,b;
+    Scanner scn = new Scanner(System.in);
+		
+		a = scn.nextInt();
+		b = scn.nextInt();
+		System.out.println("Sum = "+ (a+b));
+	}
+}
